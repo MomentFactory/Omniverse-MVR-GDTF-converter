664e1c1aea8102403690a9632090256a5f1744ff
Frederic Lestage <fredericl@momentfactory.com>
2023-06-15 18:00:33 -0400

    Add USDTool method to create template usd file

--- exts/mf.ov.gdtf/mf/ov/gdtf/USDTools.py
+++ exts/mf.ov.gdtf/mf/ov/gdtf/USDTools.py
@@ -1,5 +1,5 @@
 import omni.usd
-from pxr import Usd
+from pxr import Usd, UsdGeom
 
 
 class USDTools:
@@ -16,3 +16,15 @@ class USDTools:
         repository_path = root_layer.repositoryPath
         dir_index = repository_path.rfind("/")
         return repository_path[:dir_index + 1]
+
+    def get_or_create_stage(url: str) -> Usd.Stage:
+        try:  # TODO: Better way to check if stage exists?
+            return Usd.Stage.Open(url)
+        except:
+            stage = Usd.Stage.CreateNew(url)
+            UsdGeom.SetStageMetersPerUnit(stage, UsdGeom.LinearUnits.centimeters)  # TODO get user defaults
+            UsdGeom.SetStageUpAxis(stage, UsdGeom.Tokens.y)  # TODO get user defaults
+            default_prim = stage.DefinePrim("/World", "Xform")
+            stage.SetDefaultPrim(default_prim)
+            stage.Save()
+            return stage
