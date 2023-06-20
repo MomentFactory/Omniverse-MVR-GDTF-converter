Apply correct translation and rotation to gltf by gdtf spec

exts/mf.ov.gdtf/mf/ov/gdtf/USDTools.py
@@ -1,8 +1,9 @@
-import omni.usd
-from pxr import Usd, UsdGeom
-from typing import List
+import numpy as np
+from typing import List, Tuple
 from unidecode import unidecode
 
+import omni.usd
+from pxr import Usd, UsdGeom
 from pxr import Gf,  Tf
 
 
@@ -37,21 +38,46 @@ class USDTools:
             stage.Save()
             return stage
 
-    def add_reference(stage: Usd.Stage, ref_path_relative: str, stage_path: str, stage_subpath: str) -> UsdGeom.Xform:
-        _: UsdGeom.Xform = UsdGeom.Xform.Define(stage, stage_path)
+    def add_reference(stage: Usd.Stage, ref_path_relative: str, stage_path: str, stage_subpath: str) -> Tuple[
+            UsdGeom.Xform, UsdGeom.Xform]:
+        xform_parent: UsdGeom.Xform = UsdGeom.Xform.Define(stage, stage_path)
         xform_ref: UsdGeom.Xform = UsdGeom.Xform.Define(stage, stage_path + stage_subpath)
         xform_ref_prim: Usd.Prim = xform_ref.GetPrim()
         references: Usd.References = xform_ref_prim.GetReferences()
         references.AddReference(ref_path_relative)
-        return xform_ref
+        return xform_parent, xform_ref
 
-    def apply_xform_op(xform: UsdGeom.Xform, xformOp: UsdGeom.XformOp, value: Gf.Vec3f):
+    def apply_scale_xform_op(xform: UsdGeom.Xform, value: Gf.Vec3f):
         xform_ordered_ops: List[UsdGeom.XformOp] = xform.GetOrderedXformOps()
         found_op = False
         for xform_op in xform_ordered_ops:
-            if xform_op.GetOpType() == xformOp:
+            if xform_op.GetOpType() == UsdGeom.XformOp.TypeScale:
                 xform_op.Set(value)
                 found_op = True
 
         if not found_op:
             xform.AddScaleOp().Set(value)
+
+    def np_matrix_from_gdtf(value: str) -> np.matrix:
+        # GDTF Matrix is: 4x4, row-major, Right-Handed, Z-up (Distance Unit not specified, but mm implied)
+        # expect form like "{x,y,z,w}{x,y,z,w}{x,y,z,w}{x,y,z,w}" where "x","y","z", "w" is similar to 1.000000
+        # make source compatible with np.matrix constructor: "x y z; x y z; x y z; x y z"
+        value_alt = value[1:]  # Removes "{" prefix
+        value_alt = value_alt[:-1]  # Removes "}" suffix
+        value_alt = value_alt.replace("}{", "; ")
+        value_alt = value_alt.replace(",", " ")
+
+        np_matrix: np.matrix = np.matrix(value_alt)
+        np_matrix.transpose()
+        return np_matrix
+
+    def gf_matrix_from_gdtf(np_matrix: np.matrix, scale: float) -> Gf.Matrix4d:
+        gf_matrix = Gf.Matrix4d(
+            np_matrix.item((0, 0)), np_matrix.item((0, 1)), np_matrix.item((0, 2)), np_matrix.item((0, 3)) * scale,
+            np_matrix.item((1, 0)), np_matrix.item((1, 1)), np_matrix.item((1, 2)), np_matrix.item((1, 3)) * scale,
+            np_matrix.item((2, 0)), np_matrix.item((2, 1)), np_matrix.item((2, 2)), np_matrix.item((2, 3)) * scale,
+            np_matrix.item((3, 0)), np_matrix.item((3, 1)), np_matrix.item((3, 2)), np_matrix.item((3, 3))
+        )
+
+        # Uses transpose because gdtf is row-major and faster to write than to rewrite the whole matrix constructor
+        return gf_matrix.GetTranspose()

exts/mf.ov.gdtf/mf/ov/gdtf/gdtfImporter.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 import tempfile
 from typing import List
 import xml.etree.ElementTree as ET
@@ -126,22 +127,47 @@ class GDTFImporter:
         for geometry in geometries:
             model: Model = geometry.get_model()
             relative_path: str = stage_path.get_relative_from(model.get_converted_filepath())
-            xform: UsdGeom.Xform = USDTools.add_reference(stage, relative_path, geometry.get_stage_path(), "/model")
-            geometry.set_xform(xform)
+            xform_parent, xform_model = USDTools.add_reference(stage, relative_path, geometry.get_stage_path(),
+                                                               "/model")
+            geometry.set_xform_parent(xform_parent)
+            geometry.set_xform_model(xform_model)
         stage.Save()
 
     def _apply_gltf_scale(stage: Usd.Stage, geometries: List[GeometryAxis]):
         stage_metersPerUnit = UsdGeom.GetStageMetersPerUnit(stage)
         scale_offset = UsdGeom.LinearUnits.millimeters
         scale = scale_offset / stage_metersPerUnit
-        scaleOp = Gf.Vec3f(scale, scale, scale)
+        scaleValue = Gf.Vec3f(scale, scale, scale)
 
         for geometry in geometries:
-            xform = geometry.get_xform()
-            USDTools.apply_xform_op(xform, UsdGeom.XformOp.TypeScale, scaleOp)
+            xform = geometry.get_xform_model()
+            USDTools.apply_scale_xform_op(xform, scaleValue)
 
         stage.Save()
 
     def _apply_gdtf_matrix(stage: Usd.Stage, geometries: List[GeometryAxis]):
-        pass
+        rotate_minus90deg_xaxis = Gf.Matrix3d(1, 0, 0, 0, 0, 1, 0, -1, 0)
+        stage_scale = UsdGeom.GetStageMetersPerUnit(stage)
+        gdtf_scale = 1  # GDTF dimensions are in meters
+        applied_scale = gdtf_scale / stage_scale
+
+        for geometry in geometries:
+            xform: UsdGeom.Xform = geometry.get_xform_parent()
+            np_matrix: np.matrix = USDTools.np_matrix_from_gdtf(geometry.get_position())
+            gf_matrix: Gf.Matrix4d = USDTools.gf_matrix_from_gdtf(np_matrix, applied_scale)
+
+            rotation: Gf.Rotation = gf_matrix.ExtractRotation()
+            euler: Gf.Vec3d = rotation.Decompose(Gf.Vec3d.XAxis(), Gf.Vec3d.YAxis(), Gf.Vec3d.ZAxis())
+
+            # Z-up to Y-up
+            # TODO: Validate with stage up axis
+            translation = rotate_minus90deg_xaxis * gf_matrix.ExtractTranslation()
+            # Don't need to rotate model since the gltf import takes care of it
+            # The translation must be adjusted since it comes from the gdtf
+
+            xform.ClearXformOpOrder()  # Prevent error when overwritting
+            xform.AddTranslateOp().Set(translation)
+            xform.AddRotateXYZOp().Set(euler)
+
+        stage.Save()
     # endregion